2732272a2b6c7f41bd6f3d7597d08d145514864d
Dalleth <essi_dalleth@outlook.com>
2023-12-29 16:29:48 -0300

    creation of the experiences

--- src/app/modules/portifolio/components/experiences/experiences.component.ts
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interface/IExperiences.interface';
 
 @Component({
   selector: 'app-experiences',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      summary:{
+        strong:'Assistente Contábil',
+        p: 'Contabilidade e Consultoria | Junho 2022 - Presente'
+       },
+       text:'<p>Faço toda a parte de contábil das empresas de minha responsabilidade. </p>'
+    }
+  ])
 
 }
