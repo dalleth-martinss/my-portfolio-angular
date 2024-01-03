d7ecf4247a48550f344efb3dbb9101e815e04e8f
Dalleth <essi_dalleth@outlook.com>
2024-01-02 21:41:40 -0300

    Adding developed projects section

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,14 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
-
+//material dialog
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+//enum
+import { EDialogPanelClass } from '../../enum/EdialogPanelClass.enum';
+import { DialogProjectsComponent } from '../../dialog/dialog-projects/dialog-projects.component';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/channel.png',
@@ -25,7 +32,90 @@ export class ProjectsComponent {
       ]
 
 
-    }
+    },
+    {
+      src: 'assets/img/channel.png',
+      alt: "Vida de Front End",
+      title: 'Front-End ',
+      height: '51px',
+      width: '51px',
+      description: ' ',
+      links: [
+        {
+          name: 'Conheça meu linkedIn',
+          href: 'https://www.linkedin.com/in/dalleth-martins/'
+        }
+      ]
+
+
+    },    {
+      src: 'assets/img/channel.png',
+      alt: "Vida de Front End",
+      title: 'Front-End ',
+      height: '51px',
+      width: '51px',
+      description: ' ',
+      links: [
+        {
+          name: 'Conheça meu linkedIn',
+          href: 'https://www.linkedin.com/in/dalleth-martins/'
+        }
+      ]
+
+
+    },    {
+      src: 'assets/img/channel.png',
+      alt: "Vida de Front End",
+      title: 'Front-End ',
+      height: '51px',
+      width: '51px',
+      description: ' ',
+      links: [
+        {
+          name: 'Conheça meu linkedIn',
+          href: 'https://www.linkedin.com/in/dalleth-martins/'
+        }
+      ]
+
+
+    },    {
+      src: 'assets/img/channel.png',
+      alt: "Vida de Front End",
+      title: 'Front-End ',
+      height: '51px',
+      width: '51px',
+      description: ' ',
+      links: [
+        {
+          name: 'Conheça meu linkedIn',
+          href: 'https://www.linkedin.com/in/dalleth-martins/'
+        }
+      ]
+
+
+    },    {
+      src: 'assets/img/channel.png',
+      alt: "Vida de Front End",
+      title: 'Front-End ',
+      height: '51px',
+      width: '51px',
+      description: ' ',
+      links: [
+        {
+          name: 'Conheça meu linkedIn',
+          href: 'https://www.linkedin.com/in/dalleth-martins/'
+        }
+      ]
+
+
+    },
   ])
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 
 }
